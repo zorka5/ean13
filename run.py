706b268dd16d82703a561bc44dab4d96050dea2d
Zofia Kubrak <zofiakubrak@gmail.com>
2022-11-03 13:02:50 +0100

    remove last digit

--- run.py
+++ run.py
@@ -2,5 +2,4 @@ from ean13.barcode_generator import BarcodeGenerator
 
 # python D:\Documents\Projects\ean13\run.py
 
-b = BarcodeGenerator("8711253001202")
-# b = BarcodeGenerator("5901234123457")
+b = BarcodeGenerator("871125300120")
